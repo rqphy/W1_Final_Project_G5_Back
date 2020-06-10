166ac63adb7de6037f3f9e7a1325558eecfb8ac0
Raphael <raphael.ferreira@hetic.net>
2020-06-10 15:55:03 +0200

    cleaning admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . "/admin_co.php";
 
+$binds = [];
+
+// if name is set => user sent infos
 if (isset($_GET['name'])) {
 
     $infos = [
@@ -17,14 +20,15 @@ if (isset($_GET['name'])) {
         'video_link' => null,
         'details' => null
     ];
+    // build $binds and fill $infos with user's infos
     foreach($infos as $key => $inf) {
         if ($_GET[$key] !== '') {
             $infos[$key] = $_GET[$key];
         }
+        if ($key !== 'img_link') $binds[':'.$key] = $infos[$key];
     }
-    var_dump($infos);
     addToPrim($pdo, $infos);
-    addToSec($pdo, $infos);
+    addToSec($pdo, $binds);
 }
 
 //add to primary DB the img link and zone for one element
@@ -38,24 +42,9 @@ function addToPrim( PDO $pdo, array $infos): void{
 
 //add to secondary DB the rest of the info for one element
 function addToSec( PDO $pdo, array $infos): void{
-    $binds = [
-        ':name' => $infos['name'],
-        ':zone' => $infos['zone'],
-        ':weight' => $infos['weight'],
-        ':size' => $infos['size'],
-        ':longevity' => $infos['longevity'],
-        ':reproduction' => $infos['reproduction'],
-        ':family' => $infos['family'],
-        ':species' => $infos['species'],
-        ':map_link' => $infos['map_link'],
-        ':video_link' => $infos['video_link'],
-        ':details' => $infos['details']
-    ];
     $stmt = $pdo->prepare('
     INSERT INTO sec (name, zone, weight, size, longevity, reproduction, family, species, map_link, video_link, details)
     VALUES (:name, :zone, :weight, :size, :longevity, :reproduction, :family, :species, :map_link, :video_link, :details);
     ');
-    $stmt->execute($binds);
+    $stmt->execute($infos);
 }
-    
-// calls both functions above if test has a value
