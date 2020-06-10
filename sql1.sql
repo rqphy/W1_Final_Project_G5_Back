4e35085f7d4a13ee1f2f4c79d4e87f6105abf448
charlescsn <56915346+charlescsn@users.noreply.github.com>
2020-06-10 10:22:14 +0200

    updated sql to create db

--- sql1.sql
+++ sql1.sql
@@ -1,25 +1,27 @@
 CREATE TABLE prim (
-    id_animals INT(3) NOT NULL,
+    id_animals INT(3) NOT NULL AUTO_INCREMENT,
     img_link VARCHAR(20) NOT NULL,
+    zone INT(3) NOT NULL,
     PRIMARY KEY (id_animals)
 ) ENGINE=InnoDB ;
 
-INSERT INTO prim (id_animals, img_link) VALUES
-(0,'yvette'),
-(1,'boop'),
-(2, 'fofo'), 
-(3,'dozy'), 
-(4,'speedy'), 
-(5, 'harry'), 
-(6,'surfy'), 
-(7,'dipsy'), 
-(8,'rorry'), 
-(9,'scotty'), 
-(10, 'roberto'), 
-(11, 'popole'), 
-(12, 'pit'), 
-(13, 'max'), 
-(14, 'cookie'), 
-(15, 'plastic'), 
-(16, 'nuke'), 
-(17, 'poach');
+INSERT INTO prim (img_link, zone) VALUES
+('yvette', 0),
+('boop', 2), 
+('fofo', 1), 
+('dozy', 1), 
+('speedy', 3), 
+('harry', 1), 
+('surfy', 0), 
+('dipsy', 1), 
+('rorry', 0), 
+('scotty', 2), 
+('roberto', 2), 
+('popole', 1), 
+('pit', 2), 
+('max', 0), 
+('cookie', 0), 
+('marcus', 3),
+('plastic', 10), 
+('nuke', 10), 
+('poach', 10);
