ef394f5e54d778b9abbb44e4d098397c65303222
Raphael <raphael.ferreira@hetic.net>
2020-06-11 21:52:29 +0200

    change button content and remove useless button from main menu

--- admin/back_office.php
+++ admin/back_office.php
@@ -10,8 +10,7 @@
   <h1 class="title">Welcome to the back office</h1>
   <main class="hero">
     <a href="./pages/add.php" class="hero__link">Add To Data Base</a>
-    <a href="./pages/remove.php" class="hero__link">Remove From Data Base</a>
-    <a href="#" class="hero__link">Add To Data Base</a>
+    <a href="./pages/remove.php" class="hero__link">Edit / Delete</a>
   </main>
 </body>
 </html>
