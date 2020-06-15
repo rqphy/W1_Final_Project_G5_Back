a944dcd73d018423ebb0387072036d0b5f5a9e68
Raphael <raphael.ferreira@hetic.net>
2020-06-15 18:04:25 +0200

    correct connect.php

--- API/connect.php
+++ API/connect.php
@@ -1,5 +1,5 @@
 <?php
-// header("Content-Type: application/json");
+header("Content-Type: application/json");
 
 try{
     $pdo = new PDO("mysql:host=localhost;port=3306;dbname=fish;", "root", "");
